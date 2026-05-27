a0318ee17b1b48172fd6f70f6374226a959ed60e
KSHITISH <kshitishshahi@gmail.com>
2026-05-27 23:41:37 +0530

    Recursion : Printing number from 'N' to 1

---
+++ DSA/DAY5/Recursion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Recursion {
+
+    static void printNumbers(int n) {
+        if (n == 0)
+            return;
+
+        System.out.println(n);
+        printNumbers(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+
+        printNumbers(n);
+    }
+}
