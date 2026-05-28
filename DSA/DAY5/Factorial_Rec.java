4ffa2a6b67678d4534ecb28c5afa3e6d133c77ce
KSHITISH <kshitishshahi@gmail.com>
2026-05-28 23:30:47 +0530

    Recursion : Print Factorial

---
+++ DSA/DAY5/Factorial_Rec.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Factorial_Rec {
+
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Entre the Value of N : ");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(factorial(n));
+    }
+}
